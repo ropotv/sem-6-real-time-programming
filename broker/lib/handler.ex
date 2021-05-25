e3ea64c5d3cdf81289098e5a2c191df4aadb305a
Vasile Ropot <vasile.ropot@codefactorygroup.com>
2021-05-25 20:17:51 +0300

    update handler

--- broker/lib/handler.ex
+++ broker/lib/handler.ex
@@ -10,22 +10,24 @@ defmodule Handler do
   end
 
   defp handle_action(:content, client, data) do
-    topic = data["topic"]
     content = data["content"]
-    clients = Registry.get(topic)
 
-    for client <- clients  do
-      try do
-        TCPHelper.send(client, content)
-      rescue
-        _ -> IO.puts("Could not send data to the client, adding to the queue")
-             Queue.add(client, content)
-      end
+    try do
+      TCPHelper.send(client, content)
+    rescue
+      _ -> IO.puts("Could not send data to the client, adding to the queue")
+           Queue.add(client, content)
     end
   end
 
-  def handle(client, data) do
+  def handle(data) do
     {:ok, decoded} = Poison.decode(data)
-    handle_action(String.to_atom(decoded["action"]), client, decoded)
+    topic = decoded["topic"]
+    action = decoded["action"]
+    clients = Registry.get(topic)
+
+    for client <- clients  do
+      handle_action(String.to_atom(action), client, decoded)
+    end
   end
 end
